f44c5f12a881de5684be07638c42398ea9697f40
clelei <e0727973@student.tuwien.ac.at>
2015-06-08 22:32:42 +0200

    Invoice file creation update

--- Coordinator/src/main/java/ac/at/tuwien/wmpm/coordinator/routes/FilePolling.java
+++ Coordinator/src/main/java/ac/at/tuwien/wmpm/coordinator/routes/FilePolling.java
@@ -1,10 +1,26 @@
 package ac.at.tuwien.wmpm.coordinator.routes;
 
+import java.io.File;
 
 public class FilePolling {
+  
+  Long timeDifference = 1000L;
+  
+  public boolean accept(File file) {
+
+  long lastModified = file.lastModified();
+  long currentTime = System.currentTimeMillis();
+
+  return (currentTime - lastModified) > timeDifference ;
+  }
+
+  public void setTimeDifference(Long timeDifference) {
+       this.timeDifference = timeDifference;
+  }
 
   public FilePolling() {
-    // TODO Auto-generated constructor stub
+    
+    
   }
 
 }

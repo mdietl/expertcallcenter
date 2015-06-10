65f979a39a7ff5e45232e79840e8b1150896abbb
anna.sadriu@gmail.com <anna.sadriu@gmail.com>
2015-06-10 23:13:29 +0200

    aggregation done

--- Coordinator/src/main/java/ac/at/tuwien/wmpm/coordinator/configuration/CamelConfiguration.java
+++ Coordinator/src/main/java/ac/at/tuwien/wmpm/coordinator/configuration/CamelConfiguration.java
@@ -1,7 +1,6 @@
 package ac.at.tuwien.wmpm.coordinator.configuration;
 
 import ac.at.tuwien.wmpm.coordinator.routes.IncomingRequestRoute;
-import ac.at.tuwien.wmpm.coordinator.routes.MainRoute;
 import org.apache.camel.spring.SpringCamelContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -13,9 +12,6 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class CamelConfiguration {
-
-  @Autowired
-  private MainRoute mainRoute;
 
   @Autowired
   private IncomingRequestRoute incomingRequestRoute;
